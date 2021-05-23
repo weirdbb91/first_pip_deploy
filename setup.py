7e0b9e30f8296abb9c8e067b6c0a1cdc2b4ec4f8
weirdbb91 <weirdbb91@gmail.com>
2021-05-23 16:50:33 +0900

    add random_address util

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "weirdbb91",
-    version = "0.0.3",
+    version = "0.1.0",
+    license = 'MIT',
+    py_modules = ['weirdbb91'],
     description = "it's my first pip deploy, say hi and bye",
     author = "seungho.baek",
     author_email = "weirdbb91@gmail.com",
@@ -10,11 +12,13 @@ setup(
     download_url = "https://github.com/weirdbb91/first_pip_deploy/archive/master.zip",
     install_requires =  [],
     packages = find_packages(exclude = []),
-    keywords = ["pypi deploy"],
+    keywords = ["pypi deploy weirdbb91"],
     python_requires = ">=3",
     package_data = {},
     zip_safe = False,
     classifiers = [
-        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
 )
