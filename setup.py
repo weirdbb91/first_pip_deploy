5f649525ff430895125d417ea1583c814f94f350
weirdbb91 <weirdbb91@gmail.com>
2021-05-23 15:40:34 +0900

    typofix setup.py / version 0.0.w -> 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "weirdbb91",
-    version = "0.0.w",
+    version = "0.0.2",
     description = "it's my first pip deploy, say hi and bye",
     author = "seungho.baek",
     author_email = "weirdbb91@gmail.com",
